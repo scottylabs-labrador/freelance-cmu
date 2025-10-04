fa5dd5ceeadc446bbed359833c69c1e5d223f7dc
Mirabelle Feng <mirabellefeng@gmail.com>
2025-10-04 17:54:03 -0400

    front end edits - footer, jobbox

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,9 +3,10 @@ import { TiHeart } from "react-icons/ti";
 const Footer = () => {
   return (
     <footer className="fixed left-0 right-0 bg-blue-200 border-b border-black">
-      <div className="flex flex-row justify-end items-center pb-2 pt-2">
+      <div className="flex flex-row justify-end items-center pb-2 pt-2 px-4">
         <h1 className="text px-1">
-          made w my husband chatgpt and my divorced wife notebooklm
+          made by daniel park • mirabelle feng • sai pendekanti • elisha
+          rahardja • lili trinh
         </h1>
         <TiHeart></TiHeart>
       </div>
--- src/components/jobbox/JobBox.tsx
+++ src/components/jobbox/JobBox.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface Props {
   JobTitle: string;
   JobDescription: string;
@@ -12,9 +10,12 @@ export default function JobBox({ JobTitle, JobDescription, JobPoster }: Props) {
       <div className="flex flex-col rounded-l-2xl w-10 h-80 bg-blue-200 "></div>
       <div className="flex flex-col w-50 rounded-r-2xl px-2 py-2 bg-gray-100">
         <h1 className="text-2xl">{JobTitle}</h1>
-        <p>{JobDescription}</p>
-        <div>container for tags</div>
-        <p>{JobPoster}</p>
+        <p>==================</p>
+        <p className="h-30  bg-white">{JobDescription}</p>
+        <div className="justify-center bg-gray-200 h-30">
+          container for tags
+        </div>
+        <p className="h-10">{JobPoster}</p>
       </div>
     </div>
   );
